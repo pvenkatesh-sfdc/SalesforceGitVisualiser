e0aad5dedb77e5481c87201d4ffa97687d19b8a3
Thierry Feltin <tfeltin@salesforce.com>
2020-06-10 19:41:36 +0200

    fix wrong case
    Signed-off-by: Thierry Feltin <tfeltin@salesforce.com>

--- SFDCGitHubAuthenticator/force-app/main/default/lwc/gitHubReposList/gitHubReposList.js
+++ SFDCGitHubAuthenticator/force-app/main/default/lwc/gitHubReposList/gitHubReposList.js
@@ -2,8 +2,8 @@ import { LightningElement, wire } from 'lwc';
 import fetchGitHubRepos from '@salesforce/apex/GitHubRepoController.fetchGitHubRepos';
 
 const columns = [
-     {label: 'Repo name', fieldName: 'name__c', type: 'text'},
-     {label: 'Repo Description', fieldName: 'description__c', type: 'text'},
+     {label: 'Repo name', fieldName: 'Name__c', type: 'text'},
+     {label: 'Repo Description', fieldName: 'Description__c', type: 'text'},
 ];
 
 export default class GitHubReposList extends LightningElement {
